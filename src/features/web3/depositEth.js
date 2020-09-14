4687ea29ecff16b271c7e3512a43e46bf7197349
jin10086 <igaojin@qq.com>
2020-09-14 13:34:16 +0800

    Update depositEth.js

--- src/features/web3/depositEth.js
+++ src/features/web3/depositEth.js
@@ -14,7 +14,7 @@ export const depositEth = async ({web3, address, amount, contractAddress}) => {
       to: contractAddress,
       value: amount,
       gasPrice: web3.utils.toWei(gasPrice, 'gwei'),
-      gasLimit: 900000
+      gasLimit: 300000
     })
     .on('transactionHash', function(hash){
       resolve(hash)
