6561e5cd458cd4cbadf07b98434f372365be2d1c
iXiaowu <zhangxiaotong@yahoo.com>
2020-09-15 20:04:05 +0800

    update home snackbar i18n

--- src/features/home/HomePage.js
+++ src/features/home/HomePage.js
@@ -43,8 +43,8 @@ export default function HomePage() {
     <>
       <SnackbarContent
         message={
-            <span onClick={() => window.open("https://www.yuque.com/sawd49/faq/umt7p5")} style={{fontWeight: "bold"}}>
-              关于机枪池存token和itoken之间关系的说明
+            <span onClick={() => window.open(t('Home-SnackBarUrl'))} style={{fontWeight: "bold"}}>
+              {t('Home-SnackBarText')}
             </span>
         }
         close
