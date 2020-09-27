66751e2cbae49b587e51813e91cd9379ab782257
iXiaowu2020 <zhangxiaotong@yahoo.com>
2020-09-27 10:54:43 +0800

    update

--- src/features/farm/redux/action.js
+++ src/features/farm/redux/action.js
@@ -3,8 +3,6 @@ export { checkApproval } from './checkApproval';
 export { fetchBalance } from './fetchBalance';
 export { fetchCurrentlyStaked } from './fetchCurrentlyStaked';
 export { fetchRewardsAvailable } from './fetchRewardsAvailable';
-export { fetchHalfTime } from './fetchHalfTime';
-export { fetchCanWithdrawTime } from './fetchCanWithdrawTime';
 export { fetchApproval } from './fetchApproval';
 export { fetchStake } from './fetchStake';
 export { fetchWithdraw } from './fetchWithdraw';
--- src/features/farm/redux/initialState.js
+++ src/features/farm/redux/initialState.js
@@ -2,11 +2,14 @@ import { yCurveFiRewardsABI, balancerRewardsABI, governanceABI, pool4Abi } from 
 
 const pools = [
 	{
-		name: 'pool4',
+		name: 'YFII',
+		description: '',
 		token: 'YFII',
+		tokenDescription: '',
 		tokenDecimals: 18,
 		tokenAddress: '0xa1d0E215a23d7030842FC67cE582a6aFa3CCaB83',
 		earnedToken: 'MEFI',
+		earnedTokenDescription: '',
 		earnedTokenDecimals: 6,
 		earnedTokenAddress: '0x72Cf258c852Dc485a853370171d46B9D29fD3184',
 		earnContractAddress: '0x6A77c0c917Da188fBfa9C380f2E60dd223c0c35a',
@@ -14,10 +17,13 @@ const pools = [
 	},
 	{
 		name: 'mefilp',
+		description: '',
 		token: 'MEFI/ETH',
+		tokenDescription: '',
 		tokenDecimals: 18,
 		tokenAddress: '0xa1d0E215a23d7030842FC67cE582a6aFa3CCaB83',
 		earnedToken: 'MEFI',
+		earnedTokenDescription: '',
 		earnedTokenDecimals: 6,
 		earnedTokenAddress: '0x72Cf258c852Dc485a853370171d46B9D29fD3184',
 		earnContractAddress: '0x6CA21695CB12A251bB19aE73Bda6964f1BBc48De',
@@ -26,22 +32,18 @@ const pools = [
 ];
 
 const poolsInfo = [{
-	name: "yearn.finance",
 	staked: 0,
 	tvl: 0,
 	apy: 0,
 },{
-	name: "Balancer Pool",
 	staked: 0,
 	tvl: 0,
 	apy: 0,
 },{
-	name: "Governance",
 	staked: 0,
 	tvl: 0,
 	apy: 0,
 },{
-	name: "pool4",
 	staked: 0,
 	tvl: 0,
 	apy: 0,
@@ -51,8 +53,6 @@ const allowance = [0,0,0,0];
 const balance = [0,0,0,0];
 const currentlyStaked = [0,0,0,0];
 const rewardsAvailable = [0,0,0,0];
-const halfTime = [0,0,0,0];
-const canWithdrawTime = [0,0,0,0];
 
 
 const initialState = {
@@ -60,8 +60,6 @@ const initialState = {
 	allowance,
 	currentlyStaked,
 	rewardsAvailable,
-	halfTime,
-	canWithdrawTime,
 	balance,
 	poolsInfo,
 	fetchPoolsInfoPending: false,
@@ -69,8 +67,6 @@ const initialState = {
 	fetchBalancePending: [false,false,false,false],
 	fetchCurrentlyStakedPending: [false,false,false,false],
 	fetchRewardsAvailablePending: [false,false,false,false],
-	fetchHalfTimePending: [false,false,false,false],
-	fetchCanWithdrawTimePending: [false,false,false,false],
 	fetchApprovalPending: [false,false,false,false],
 	fetchStakePending: [false,false,false,false],
 	fetchWithdrawPending: [false,false,false,false],
