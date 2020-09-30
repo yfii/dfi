b52ac7d7398e7ec6d99fae9f1913f1d3f53d3f30
sirbeefalot <sirbeefalot@pm.me>
2020-09-29 21:01:14 -0300

    Temporarily hide Farm from homepage

--- src/features/home/HomePage.js
+++ src/features/home/HomePage.js
@@ -4,7 +4,6 @@ import { useTranslation } from 'react-i18next';
 import SnackbarContent from 'components/Snackbar/SnackbarContent.js';
 import SectionPools from 'features/vault/sections/SectionPools.js';
 import StakePage from 'features/stake/sections/StakePools.js';
-import FarmPools from 'features/farm/sections/FarmPools';
 
 export default function HomePage() {
   const { t } = useTranslation();
@@ -16,7 +15,6 @@ export default function HomePage() {
 
   return (
     <>
-      <FarmPools fromPage='home' />
       <SectionPools fromPage='home' />
       <StakePage fromPage='home' />
     </>
