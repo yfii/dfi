52e44fb3e5a7a0db33130536beae1ea2a105524c
sirbeefalot <sirbeefalot@pm.me>
2020-09-30 13:02:01 -0300

    Replace title and favicon

--- src/components/CustomSlider/CustomSlider.js
+++ src/components/CustomSlider/CustomSlider.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 import Slider from '@material-ui/core/Slider';
 import { makeStyles } from '@material-ui/core/styles';
 
