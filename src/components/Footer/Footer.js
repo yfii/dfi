fb897f6d014a226ca61098ac645f9f5a546b1c36
sirbeefalot <sirbeefalot@pm.me>
2020-10-01 19:05:42 -0300

    Masively simplify the Footer.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
 import { makeStyles } from '@material-ui/core/styles';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMediumM, faTelegramPlane, faDiscord, faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
@@ -14,33 +12,21 @@ export default function Footer(props) {
 
   return (
     <div className={classes.container}>
-      <List className={classes.list}>
-        <ListItem>
-          <a className={classes.navLink} href="https://twitter.com/beefyfinance" target="_blank">
-            <FontAwesomeIcon className={classes.socialIcons} icon={faTwitter} />
-          </a>
-        </ListItem>
-        <ListItem>
-          <a className={classes.navLink} href="https://t.me/beefyfinance" target="_blank">
-            <FontAwesomeIcon className={classes.socialIcons} icon={faTelegramPlane} />
-          </a>
-        </ListItem>
-        <ListItem>
-          <a className={classes.navLink} href="https://discord.gg/yq8wfHd" target="_blank">
-            <FontAwesomeIcon className={classes.socialIcons} icon={faDiscord} />
-          </a>
-        </ListItem>
-        <ListItem>
-          <a className={classes.navLink} href="https://medium.com/beefyfinance" target="_blank">
-            <FontAwesomeIcon className={classes.socialIcons} icon={faMediumM} />
-          </a>
-        </ListItem>
-        <ListItem>
-          <a className={classes.navLink} href="https://github.com/beefyfinance" target="_blank">
-            <FontAwesomeIcon className={classes.socialIcons} icon={faGithub} />
-          </a>
-        </ListItem>
-      </List>
+      <a href="https://twitter.com/beefyfinance" target="_blank">
+        <FontAwesomeIcon className={classes.socialIcons} icon={faTwitter} />
+      </a>
+      <a href="https://t.me/beefyfinance" target="_blank">
+        <FontAwesomeIcon className={classes.socialIcons} icon={faTelegramPlane} />
+      </a>
+      <a href="https://discord.gg/yq8wfHd" target="_blank">
+        <FontAwesomeIcon className={classes.socialIcons} icon={faDiscord} />
+      </a>
+      <a href="https://medium.com/beefyfinance" target="_blank">
+        <FontAwesomeIcon className={classes.socialIcons} icon={faMediumM} />
+      </a>
+      <a href="https://github.com/beefyfinance" target="_blank">
+        <FontAwesomeIcon className={classes.socialIcons} icon={faGithub} />
+      </a>
     </div>
   );
 }
