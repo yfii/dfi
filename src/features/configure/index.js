4f54a94c43fab301558a1bb45d96afdaee99abf7
sirbeefalot <sirbeefalot@pm.me>
2020-09-30 21:18:43 -0300

    Remove unused supportedChains feature

--- src/features/configure/index.js
+++ src/features/configure/index.js
@@ -1,3 +1,16 @@
-export { earnContractABI, USDTABI, erc20ABI, yCurveZapABI, yCurveZapOutABI, yCurveZapV4ABI, yCurveZapOutV4ABI, yCurveZapSwapABI, yCurveZapSwapV4ABI, yCurveFiRewardsABI, balancerRewardsABI, governanceABI, pool4Abi } from './abi';
+export {
+  earnContractABI,
+  USDTABI,
+  erc20ABI,
+  yCurveZapABI,
+  yCurveZapOutABI,
+  yCurveZapV4ABI,
+  yCurveZapOutV4ABI,
+  yCurveZapSwapABI,
+  yCurveZapSwapV4ABI,
+  yCurveFiRewardsABI,
+  balancerRewardsABI,
+  governanceABI,
+  feeRewardsPoolAbi,
+} from './abi';
 export { pools } from './pools';
-export { supportedChains } from './chains';
--- src/features/stake/redux/initialState.js
+++ src/features/stake/redux/initialState.js
@@ -1,102 +1,56 @@
-import { yCurveFiRewardsABI, balancerRewardsABI, governanceABI, pool4Abi } from "../../configure";
+import { feeRewardsPoolAbi } from '../../configure';
 
 const pools = [
-	{
-		name: 'yearn.finance',
-		token: 'curve.fi',
-		tokenDecimals: 18,
-		tokenAddress: '0xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8',
-		earnedToken: 'YFII',
-		earnedTokenDecimals: 18,
-		earnedTokenAddress: '0xa1d0E215a23d7030842FC67cE582a6aFa3CCaB83',
-		earnContractAddress: '0xb81D3cB2708530ea990a287142b82D058725C092',
-		earnContractAbi: yCurveFiRewardsABI,
-	},
-	{
-		name: 'Balancer Pool',
-		token: 'BPT',
-		tokenDecimals: 18,
-		tokenAddress: '0x16cAC1403377978644e78769Daa49d8f6B6CF565',
-		earnedToken: 'YFII',
-		earnedTokenDecimals: 18,
-		earnedTokenAddress: '0xa1d0E215a23d7030842FC67cE582a6aFa3CCaB83',
-		earnContractAddress: '0xAFfcD3D45cEF58B1DfA773463824c6F6bB0Dc13a',
-		earnContractAbi: balancerRewardsABI,
-	},
-	{
-		name: 'Governance',
-		token: 'YFII',
-		tokenDecimals: 18,
-		tokenAddress: '0xa1d0E215a23d7030842FC67cE582a6aFa3CCaB83',
-		earnedToken: 'yCrv',
-		earnedTokenDecimals: 18,
-		earnedTokenAddress: '0xa1d0E215a23d7030842FC67cE582a6aFa3CCaB83',
-		earnContractAddress: '0xf1750B770485A5d0589A6ba1270D9FC354884D45',
-		earnContractAbi: governanceABI,
-	},
-	{
-		name: 'pool4',
-		token: 'YFII',
-		tokenDecimals: 18,
-		tokenAddress: '0xa1d0E215a23d7030842FC67cE582a6aFa3CCaB83',
-		earnedToken: 'iUSDT',
-		earnedTokenDecimals: 6,
-		earnedTokenAddress: '0x72Cf258c852Dc485a853370171d46B9D29fD3184',
-		earnContractAddress: '0x3d367c9529f260b0661e1c1e91167c9319ee96ca',
-		earnContractAbi: pool4Abi
-	},
+  {
+    name: 'pool1',
+    token: 'BIFI',
+    tokenDecimals: 18,
+    tokenAddress: '0xCa3F508B8e4Dd382eE878A314789373D80A5190A',
+    earnedToken: 'BUSD',
+    earnedTokenDecimals: 18,
+    earnedTokenAddress: '0x1ca0e4dE7938F6F194831398e782cfbba3549e8D',
+    earnContractAddress: '0x1ca0e4dE7938F6F194831398e782cfbba3549e8D',
+    earnContractAbi: feeRewardsPoolAbi,
+  },
 ];
 
-const poolsInfo = [{
-	name: "yearn.finance",
-	staked: 0,
-	tvl: 0,
-	apy: 0,
-},{
-	name: "Balancer Pool",
-	staked: 0,
-	tvl: 0,
-	apy: 0,
-},{
-	name: "Governance",
-	staked: 0,
-	tvl: 0,
-	apy: 0,
-},{
-	name: "pool4",
-	staked: 0,
-	tvl: 0,
-	apy: 0,
-}]
-const allowance = [0,0,0,0];
-const balance = [0,0,0,0];
-const currentlyStaked = [0,0,0,0];
-const rewardsAvailable = [0,0,0,0];
-const halfTime = [0,0,0,0];
-const canWithdrawTime = [0,0,0,0];
+const poolsInfo = [
+  {
+    name: 'pool4',
+    staked: 0,
+    tvl: 0,
+    apy: 0,
+  },
+];
 
+const allowance = [0, 0, 0, 0];
+const balance = [0, 0, 0, 0];
+const currentlyStaked = [0, 0, 0, 0];
+const rewardsAvailable = [0, 0, 0, 0];
+const halfTime = [0, 0, 0, 0];
+const canWithdrawTime = [0, 0, 0, 0];
 
 const initialState = {
-	pools,
-	allowance,
-	currentlyStaked,
-	rewardsAvailable,
-	halfTime,
-	canWithdrawTime,
-	balance,
-	poolsInfo,
-	fetchPoolsInfoPending: false,
-	checkApprovalPending: [false,false,false,false],
-	fetchBalancePending: [false,false,false,false],
-	fetchCurrentlyStakedPending: [false,false,false,false],
-	fetchRewardsAvailablePending: [false,false,false,false],
-	fetchHalfTimePending: [false,false,false,false],
-	fetchCanWithdrawTimePending: [false,false,false,false],
-	fetchApprovalPending: [false,false,false,false],
-	fetchStakePending: [false,false,false,false],
-	fetchWithdrawPending: [false,false,false,false],
-	fetchClaimPending: [false,false,false,false],
-	fetchExitPending: [false,false,false,false]
+  pools,
+  allowance,
+  currentlyStaked,
+  rewardsAvailable,
+  halfTime,
+  canWithdrawTime,
+  balance,
+  poolsInfo,
+  fetchPoolsInfoPending: false,
+  checkApprovalPending: [false, false, false, false],
+  fetchBalancePending: [false, false, false, false],
+  fetchCurrentlyStakedPending: [false, false, false, false],
+  fetchRewardsAvailablePending: [false, false, false, false],
+  fetchHalfTimePending: [false, false, false, false],
+  fetchCanWithdrawTimePending: [false, false, false, false],
+  fetchApprovalPending: [false, false, false, false],
+  fetchStakePending: [false, false, false, false],
+  fetchWithdrawPending: [false, false, false, false],
+  fetchClaimPending: [false, false, false, false],
+  fetchExitPending: [false, false, false, false],
 };
 
 export default initialState;
