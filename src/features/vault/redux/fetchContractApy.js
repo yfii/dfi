8dfd0d177580bfe6a665b5db9eb67a0dde81f289
superbeefyboy <superbeefyboy@pm.me>
2020-10-02 17:45:31 -0300

    restored old apy calc

--- src/features/vault/redux/fetchContractApy.js
+++ src/features/vault/redux/fetchContractApy.js
@@ -10,7 +10,43 @@ import {
 // Rekit uses redux-thunk for async actions by default: https://github.com/gaearon/redux-thunk
 // If you prefer redux-saga, you can use rekit-plugin-redux-saga: https://github.com/supnate/rekit-plugin-redux-saga
 export function fetchContractApy() {
-  return null;
+  return dispatch => {
+    // optionally you can have getState as the second argument
+    dispatch({
+      type: VAULT_FETCH_CONTRACT_APY_BEGIN,
+    });
+
+    // Return a promise so that you could control UI flow without states in the store.
+    // For example: after submit a form, you need to redirect the page to another when succeeds or show some errors message if fails.
+    // It's hard to use state to manage it, but returning a promise allows you to easily achieve it.
+    // e.g.: handleSubmit() { this.props.actions.submitForm(data).then(()=> {}).catch(() => {}); }
+    const promise = new Promise((resolve, reject) => {
+      // doRequest is a placeholder Promise. You should replace it with your own logic.
+      // See the real-word example at:  https://github.com/supnate/rekit/blob/master/src/features/home/redux/fetchRedditReactjsList.js
+      // args.error here is only for test coverage purpose.
+      const doRequest = axios.get('https://api.dfi.money/apy.json');
+
+      doRequest.then(
+        res => {
+          dispatch({
+            type: VAULT_FETCH_CONTRACT_APY_SUCCESS,
+            data: res.data,
+          });
+          resolve(res);
+        },
+        // Use rejectHandler as the second argument so that render errors won't be caught.
+        err => {
+          dispatch({
+            type: VAULT_FETCH_CONTRACT_APY_FAILURE,
+            data: { error: err },
+          });
+          reject(err);
+        },
+      );
+    });
+
+    return promise;
+  };
 }
 
 export function useFetchContractApy() {
