b2cc74d23ac03b324a4a3eba0fa64ce2a2fca49c
superbeefyboy <superbeefyboy@pm.me>
2020-10-02 21:23:04 -0300

    yeet!

--- src/common/routeConfig.js
+++ src/common/routeConfig.js
@@ -1,31 +1,18 @@
 import { App } from '../features/home';
-import { PageNotFound } from '../features/common';
 import homeRoute from '../features/home/route';
-import vaultRoute from '../features/vault/route';
-import stakeRoute from '../features/stake/route';
-import farmRoute from '../features/farm/route';
-import commonRoute from '../features/common/route';
-import zapRoute from '../features/swap/route';
 import _ from 'lodash';
 
 // NOTE: DO NOT CHANGE the 'childRoutes' name and the declaration pattern.
 // This is used for Rekit cmds to register routes config for new features, and remove config when remove features, etc.
 const childRoutes = [
   homeRoute,
-  // vaultRoute,
-  // stakeRoute,
-  // commonRoute,
-  // farmRoute
-  // zapRoute,
 ];
 
 const routes = [
   {
     path: '/',
     component: App,
-    childRoutes: [...childRoutes, { path: '*', name: 'Page not found', component: PageNotFound }].filter(
-      r => r.component || (r.childRoutes && r.childRoutes.length > 0)
-    ),
+    childRoutes: childRoutes,
   },
 ];
 
--- src/features/common/index.js
+++ src/features/common/index.js
@@ -1,2 +1,1 @@
-export { default as PageNotFound } from './PageNotFound';
 export { default as Notifier } from './Notifier';
