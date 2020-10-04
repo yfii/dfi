190fd2eb71cbb3021a3a45cbf6c35bb17c602314
superbeefyboy <superbeefyboy@pm.me>
2020-10-04 18:12:08 -0300

    bsc gasprice w/a

--- src/features/web3/fetchGasPrice.js
+++ src/features/web3/fetchGasPrice.js
@@ -1,16 +1,3 @@
-import axios from 'axios';
-
 export const fetchGasPrice = async () => {
-  try {
-    const url = 'https://www.gasnow.org/api/v3/gas/price?utm_source=dfi.money';
-    // const url = 'https://gasprice.poa.network/'
-    const { data } = await axios.get(url);
-    // console.log(data.fast.toFixed(0));
-    // return data.fast.toFixed(0).toString() || '70';
-    // console.log(data.data.fast.toString())
-    return data.data.fast.toString() || '70';
-  } catch(error) {
-    // console.log(error)
-    return '70';
-  }
+  return '20';
 }
