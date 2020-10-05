23e17b19be0111bba52b9321b53f59790792b2f5
superbeefyboy <superbeefyboy@pm.me>
2020-10-05 16:43:20 -0300

    simplified abis

--- src/features/configure/index.js
+++ src/features/configure/index.js
@@ -1,12 +1,5 @@
 export {
   earnContractABI,
-  erc20ABI,
-  yCurveZapABI,
-  yCurveZapOutABI,
-  yCurveZapV4ABI,
-  yCurveZapOutV4ABI,
-  yCurveZapSwapABI,
-  yCurveZapSwapV4ABI,
-  feeRewardsPoolAbi,
+  erc20ABI
 } from './abi';
 export { pools } from './pools';
