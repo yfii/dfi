63966b974fdea5fcb596c1d09190382492b18cec
jin10086 <igaojin@qq.com>
2020-10-07 12:43:14 +0800

    add eth/usdt lp

---
+++ src/features/configure/lps.js
@@ -0,0 +1,37 @@
+// id: '池子id',
+// name: '池子名字',  
+// token: '池子代币',
+// tokenDescription: '代币描述',
+// tokenAddress: '代币ERC20地址',
+// earnedToken: '奖励代币',
+// earnedTokenAddress: '奖励代币ERC20地址',
+// earnContractAddress: '池子合约地址',
+// price ： 挖的代币的价格！
+// path price: 
+export const lps = [
+    {
+      id: 'eth/usdt',
+      name: 'eth/usdt lp',  
+      token: 'eth/usdt lp',
+      tokenDescription: 'eth/usdt lp',
+      //lp token
+      tokenAddress: '0x0d4a11d5eeaac28ec3f61d100daf4d40471f1852',
+      tokenDecimals: 18, 
+
+      //token0 
+      token0Address: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+      token0Decimals: 18, 
+      //token1
+      token1Address: '0xdac17f958d2ee523a2206206994597c13d831ec7',
+      token1Decimals: 6,
+
+      tokenDescriptionUrl: '',
+      tokenDescriptionUrl2: '',
+      earnedToken: 'iETH/USDT',
+      earnedTokenAddress: '0x7E43210a4c6831D421f57026617Fdfc8ED3A0baf',
+      earnContractAddress: '0x7E43210a4c6831D421f57026617Fdfc8ED3A0baf',
+      defaultApy: "39.54",
+      pricePerFullShare: 1,
+      pastPricePerFullShare: 1
+    }
+  ]
