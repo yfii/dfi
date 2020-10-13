49fb55bb452042113626a93384e881897e9603ab
shellteo <shellteo@163.com>
2020-10-13 16:50:43 +0800

    add usdc wbtc dai token

--- src/features/liquidity/config/erc20Tokens.js
+++ src/features/liquidity/config/erc20Tokens.js
@@ -28,5 +28,72 @@ export const erc20Tokens = {
     tockeDescription: 'LP',
     tokenDecimals: 18,
     tokenBalance: new BigNumber(0)
-  }
+  },
+  // eth/dai lp
+  "dai": {
+    tokenContractAddress: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+    tockeDescriptionUrl: 'DAI',
+    tockeDescription: 'DAI',
+    tokenDecimals: 18,
+    tokenBalance: new BigNumber(0)
+  },
+  "eth/dai lp": {
+    tokenContractAddress: '0xa478c2975ab1ea89e8196811f51a7b7ade33eb11',
+    tockeDescriptionUrl: 'LP',
+    tockeDescription: 'LP',
+    tokenDecimals: 18,
+    tokenBalance: new BigNumber(0)
+  },
+  "iETH/DAI": {
+    tokenContractAddress: '0x19d994471D61d36FE367928Cc58102a376089D1f',
+    tockeDescriptionUrl: 'LP',
+    tockeDescription: 'LP',
+    tokenDecimals: 18,
+    tokenBalance: new BigNumber(0)
+  },
+  // eth/usdc lp
+  "usdc": {
+    tokenContractAddress: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+    tockeDescriptionUrl: 'USDC',
+    tockeDescription: 'USDC',
+    tokenDecimals: 6,
+    tokenBalance: new BigNumber(0)
+  },
+  "eth/usdc lp": {
+    tokenContractAddress: '0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc',
+    tockeDescriptionUrl: 'LP',
+    tockeDescription: 'LP',
+    tokenDecimals: 18,
+    tokenBalance: new BigNumber(0)
+  },
+  "iETH/USDC": {
+    tokenContractAddress: '0x68A23248000d5d4C943EE685989998c1B19bD74E',
+    tockeDescriptionUrl: 'LP',
+    tockeDescription: 'LP',
+    tokenDecimals: 18,
+    tokenBalance: new BigNumber(0)
+  },
+  // eth/wbtc lp
+  "wbtc": {
+    tokenContractAddress: '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599',
+    tockeDescriptionUrl: 'wBTC',
+    tockeDescription: 'wBTC',
+    tokenDecimals: 8,
+    tokenBalance: new BigNumber(0)
+  },
+  "eth/wbtc lp": {
+    tokenContractAddress: '0xBb2b8038a1640196FbE3e38816F3e67Cba72D940',
+    tockeDescriptionUrl: 'LP',
+    tockeDescription: 'LP',
+    tokenDecimals: 18,
+    tokenBalance: new BigNumber(0)
+  },
+  "iETH/wBTC": {
+    tokenContractAddress: '0xb918368082655fA223c162266ecd88Aa7Ae40bc9',
+    tockeDescriptionUrl: 'LP',
+    tockeDescription: 'LP',
+    tokenDecimals: 18,
+    tokenBalance: new BigNumber(0)
+  },
+
 }
