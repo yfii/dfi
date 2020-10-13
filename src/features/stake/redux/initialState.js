9cda729970b822e6ad252ed410b223751fadab7d
jin10086 <igaojin@qq.com>
2020-10-13 23:49:01 +0800

    add pool5

--- src/features/stake/redux/initialState.js
+++ src/features/stake/redux/initialState.js
@@ -45,6 +45,17 @@ const pools = [
 		earnContractAddress: '0x3d367c9529f260b0661e1c1e91167c9319ee96ca',
 		earnContractAbi: pool4Abi
 	},
+	{
+		name: 'pool5',
+		token: 'YFII',
+		tokenDecimals: 18,
+		tokenAddress: '0xa1d0E215a23d7030842FC67cE582a6aFa3CCaB83',
+		earnedToken: 'YFII',
+		earnedTokenDecimals: 19,
+		earnedTokenAddress: '0xa1d0E215a23d7030842FC67cE582a6aFa3CCaB83',
+		earnContractAddress: '0xB6C7Bf515828c1eBf0D5F34930Cda0741eD9263F',
+		earnContractAbi: pool4Abi
+	},
 ];
 
 const poolsInfo = [{
