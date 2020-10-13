43c78624c1e4ee1e03445403fb8a3bc16cfcab7e
jin10086 <igaojin@qq.com>
2020-10-13 16:11:49 +0800

    add eth/usdc eth/dai eth/wbtc

--- src/features/configure/lps.js
+++ src/features/configure/lps.js
@@ -35,5 +35,84 @@ export const lps = [
       defaultApy: "39.54",
       pricePerFullShare: 1,
       pastPricePerFullShare: 1
-    }
+    },
+    {
+      id: 'eth/usdc',
+      name: 'eth/usdc lp',  
+      token: 'eth/usdc lp',
+      tokenDescription: 'eth/usdc lp',
+      //lp token
+      tokenAddress: '0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc',
+      tokenDecimals: 18, 
+
+      //token0 
+      token0Address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+      token0Decimals: 6, 
+      //token1
+      token1Address: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+      token1Decimals: 18,
+
+      haveETH:true,
+
+      tokenDescriptionUrl: '',
+      tokenDescriptionUrl2: '',
+      earnedToken: 'iETH/USDC',
+      earnedTokenAddress: '0x68A23248000d5d4C943EE685989998c1B19bD74E',
+      earnContractAddress: '0x68A23248000d5d4C943EE685989998c1B19bD74E',
+      defaultApy: "39.54",
+      pricePerFullShare: 1,
+      pastPricePerFullShare: 1
+    },{
+      id: 'eth/dai',
+      name: 'eth/dai lp',  
+      token: 'eth/dai lp',
+      tokenDescription: 'eth/dai lp',
+      //lp token
+      tokenAddress: '0xa478c2975ab1ea89e8196811f51a7b7ade33eb11',
+      tokenDecimals: 18, 
+
+      //token0 
+      token0Address: '0x6b175474e89094c44da98b954eedeac495271d0f',
+      token0Decimals: 18, 
+      //token1
+      token1Address: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+      token1Decimals: 18,
+
+      haveETH:true,
+
+      tokenDescriptionUrl: '',
+      tokenDescriptionUrl2: '',
+      earnedToken: 'iETH/DAI',
+      earnedTokenAddress: '0x19d994471D61d36FE367928Cc58102a376089D1f',
+      earnContractAddress: '0x19d994471D61d36FE367928Cc58102a376089D1f',
+      defaultApy: "39.54",
+      pricePerFullShare: 1,
+      pastPricePerFullShare: 1
+    },{
+      id: 'eth/wbtc',
+      name: 'eth/wbtc lp',  
+      token: 'eth/wbtc lp',
+      tokenDescription: 'eth/wbtc lp',
+      //lp token
+      tokenAddress: '0xbb2b8038a1640196fbe3e38816f3e67cba72d940',
+      tokenDecimals: 18, 
+
+      //token0 
+      token0Address: '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
+      token0Decimals: 8, 
+      //token1
+      token1Address: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+      token1Decimals: 18,
+
+      haveETH:true,
+
+      tokenDescriptionUrl: '',
+      tokenDescriptionUrl2: '',
+      earnedToken: 'iETH/WBTC',
+      earnedTokenAddress: '0xb918368082655fA223c162266ecd88Aa7Ae40bc9',
+      earnContractAddress: '0xb918368082655fA223c162266ecd88Aa7Ae40bc9',
+      defaultApy: "39.54",
+      pricePerFullShare: 1,
+      pastPricePerFullShare: 1
+    },
   ]
