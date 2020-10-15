3890f1a94c96d1d30a235a91e65f6d3c62c331a6
LancelotM <yxm134679@163.com>
2020-10-15 19:09:06 +0800

    #Add Liquidity SnackbarContent

--- src/features/liquidity/LiquidityPage.js
+++ src/features/liquidity/LiquidityPage.js
@@ -1,19 +1,31 @@
 import React, { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 // @material-ui/core components
 // @material-ui/icons
 // import Close from "@material-ui/icons/Close";
 // core components
 // sections for this page
 import { SectionTitle, SectionPools } from '.';
+import SnackbarContent from "components/Snackbar/SnackbarContent.js";
 // hooks
 
 export default function LiquidityPage() {
+  const { t, i18n } = useTranslation();
   useEffect(() => {
     window.scrollTo(0, 0);
     document.body.scrollTop = 0;
   }, []);
   return (
     <>
+      <SnackbarContent
+        message={
+            <span onClick={() => window.open(t('Liquidity-SnackBarUrl'))} style={{fontWeight: "bold"}}>
+              {t('Liquidity-SnackBarText')}
+            </span>
+        }
+        close
+        color="success"
+      />
       <SectionTitle />
       <SectionPools />
     </>
