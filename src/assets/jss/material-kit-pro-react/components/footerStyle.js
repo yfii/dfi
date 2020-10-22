f71926f3386cdc5968f266c9d39018b0e46e1a61
sirbeefalot <sirbeefalot@pm.me>
2020-10-21 21:18:18 -0300

    Remove styled-components from Footer

---
+++ src/assets/jss/material-kit-pro-react/components/footerStyle.js
@@ -0,0 +1,36 @@
+const footerStyle = theme => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    maxWidth: '40rem',
+    margin: '2rem auto',
+    '@media (min-width: 769px)': {
+      margin: '2rem auto 10rem',
+    },
+  },
+  column: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  title: {
+    fontWeight: 900,
+    fontSize: '1.2rem',
+    marginBottom: '0.5rem',
+  },
+  link: {
+    margin: '0.5rem 0',
+    fontWeight: 400,
+    color: '#000',
+    textDecoration: 'none',
+    '&:hover': {
+      textDecoration: 'underline',
+    },
+  },
+  linkIcon: {
+    marginRight: '0.5rem',
+    minWidth: '24px',
+  },
+});
+
+export default footerStyle;
