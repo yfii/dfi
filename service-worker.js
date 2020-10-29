15686472b7c79fcad8a6ae04f62fee9003480a0c
jin10086 <igaojin@qq.com>
2020-10-29 13:30:29 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8bd24befd5db7f478d29a33cf485a643.js"
+  "/precache-manifest.33b6b5f6f46f98ba8ae2b5ab91113a9d.js"
 );
 
 self.addEventListener('message', (event) => {
