db23ba0610deaf75018913e7ce6a881dc63af098
superbeefyboy <superbeefyboy@pm.me>
2020-11-07 19:43:43 -0300

    last cache refresh :)

--- src/features/vault/redux/fetchContractApy.js
+++ src/features/vault/redux/fetchContractApy.js
@@ -20,7 +20,7 @@ export function fetchContractApy() {
       // doRequest is a placeholder Promise. You should replace it with your own logic.
       // See the real-word example at:  https://github.com/supnate/rekit/blob/master/src/features/home/redux/fetchRedditReactjsList.js
       // args.error here is only for test coverage purpose.
-      const doRequest = axios.get('https://beefy-api.herokuapp.com/apy');
+      const doRequest = axios.get('https://beefy-api.herokuapp.com/apy?_=1604789002');
 
       doRequest.then(
         res => {
