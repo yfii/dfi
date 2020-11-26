00acbb4d2b84bc1a71b3244862fd294d1a716465
superbeefyboy <superbeefyboy@pm.me>
2020-11-26 04:25:42 -0300

    proper margin

--- src/features/vault/components/Filters/styles.js
+++ src/features/vault/components/Filters/styles.js
@@ -1,8 +1,9 @@
 const styles = theme => ({
   container: {
     padding: '24px',
-    marginBottom: '2rem',
+    margin: '8px 0 2rem',
     border: '1px solid #DED9D5',
+    backgroundColor: '#FBF6F0',
     justifyContent: 'space-between',
   },
 
