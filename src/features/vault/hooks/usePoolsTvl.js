f1cce5023895aaaac1c7bae80e201ecd80602d10
RM <roman-monk@users.noreply.github.com>
2020-12-16 14:32:37 +0800

    Exclude duplicate contracts from TVL

--- src/features/vault/hooks/usePoolsTvl.js
+++ src/features/vault/hooks/usePoolsTvl.js
@@ -6,7 +6,13 @@ const usePoolsTvl = pools => {
   useEffect(() => {
     let globalTvl = 0;
 
+    const isUniqueEarnContract = ((pool, index, pools) => {
+      const earnContractAddress = pool.earnContractAddress
+      return pools.findIndex(p => p.earnContractAddress === earnContractAddress) === index
+    });
+
     pools.filter(p => p.status === 'active')
+      .filter(isUniqueEarnContract)
       .forEach(({ tvl, oraclePrice, fallbackPrice }) => {
         globalTvl += tvl * (oraclePrice || fallbackPrice);
       });
