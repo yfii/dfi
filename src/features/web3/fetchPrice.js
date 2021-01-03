89f95521786b803148dcd2a3f57e2e9ad6206722
shibacrypt <>
2021-01-03 10:56:17 +0900

    Reduce cache timeout to 1 minute

--- src/features/web3/fetchPrice.js
+++ src/features/web3/fetchPrice.js
@@ -18,7 +18,7 @@ const WBNB = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c';
 const BUSD = '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56';
 const WBNB_BUSD = `${WBNB}_${BUSD}`;
 
-const CACHE_TIMEOUT_MS = 30 * 60 * 1000; // 30 minutes
+const CACHE_TIMEOUT_MS = 1 * 60 * 1000; // 1 minute(s)
 const priceCache = {
   cache: new Map(),
   lastUpdated: undefined
