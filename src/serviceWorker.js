437fa0da8ce13efc2c8e72de7adc398daa95b55d
Jon Stevens <latchkey@gmail.com>
2021-01-21 17:30:15 -0800

    unregister() returns a promise, which should also be returned

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -119,7 +119,7 @@ export function unregister() {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.ready
       .then(registration => {
-        registration.unregister();
+        return registration.unregister();
       })
       .catch(error => {
         console.error(error.message);
