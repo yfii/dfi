b9d474f125cb07be1e50d7699dcb91f9309065d4
jpopxfile <raynold.sim@gmail.com>
2021-01-27 00:43:18 +0900

    Fix spacing to 2

--- scripts/updatePools.js
+++ scripts/updatePools.js
@@ -25,7 +25,7 @@ async function main() {
         arr.push(pools[i]);
     }
 
-    const payload = `export const pools = ${JSON.stringify((arr),null,1)};`
+    const payload = `export const pools = ${JSON.stringify((arr),null,2)};`
 
     fs.writeFile('../src/features/configure/pools.js', payload, function (err) {
         if (err) return console.log(err);
