8844c5140ec7aa49a40aa3273d5c046b1bf3dc9c
superbeefyboy <superbeefyboy@pm.me>
2021-02-22 18:29:17 -0300

    casing quickfix

--- src/components/HeaderLinks/HeaderLinks.js
+++ src/components/HeaderLinks/HeaderLinks.js
@@ -116,7 +116,7 @@ const HeaderLinks = ({ connected, address, connectWallet, disconnectWallet, isNi
             'Italiano',
             '한글',
             'Português',
-            'русский',
+            'Pусский',
             'Svenska',
             'Türkçe',
             'Українська',
