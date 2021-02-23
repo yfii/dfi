de974b1e17aff77e003183dbeaa0220b475562d9
Norton Wang <1502552+FloorLamp@users.noreply.github.com>
2021-02-23 22:55:50 +0700

    Save visible count in state

--- src/features/vault/hooks/useVisiblePools.js
+++ src/features/vault/hooks/useVisiblePools.js
@@ -1,26 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 const useVisiblePools = (pools, chunk) => {
-  const [ visiblePools, setVisiblePools ] = useState([]);
+  const [visibleCount, setVisibleCount] = useState(chunk);
 
-  useEffect(() => {
-    // Attempt to load as many pools as there were already loaded
-    // this should stop the window jumping to the top when pools
-    // are updated.
-    setVisiblePools((previousVisiblePools) => (
-      pools.slice(0, Math.max(previousVisiblePools.length, chunk))
-    ));
-  }, [pools, chunk]);
+  let visiblePools = pools.slice(0, visibleCount);
 
   const fetchVisiblePools = () => {
-    const poolCount    = pools.length;
-    const visibleCount = visiblePools.length;
-
-    if(visibleCount >= poolCount) return;
+    if (visibleCount >= pools.length) return;
 
     // Concat visible pools with new chunk
     const newPools = pools.slice(visibleCount, visibleCount + chunk);
-    setVisiblePools(visiblePools.concat(newPools));
+    visiblePools = visiblePools.concat(newPools);
+    setVisibleCount(visibleCount + chunk);
   };
 
   return { visiblePools, fetchVisiblePools };
