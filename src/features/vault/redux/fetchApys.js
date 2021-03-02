77fb962ee8080fe2dd0710539e85720d87c9ae98
superbeefyboy <superbeefyboy@pm.me>
2021-03-02 09:18:18 -0300

    cache busting str

--- src/features/vault/redux/fetchApys.js
+++ src/features/vault/redux/fetchApys.js
@@ -14,7 +14,7 @@ export function fetchApys() {
     });
 
     const promise = new Promise((resolve, reject) => {
-      const doRequest = axios.get(`https://api.beefy.finance/apy`);
+      const doRequest = axios.get(`https://api.beefy.finance/apy?_=1614687410`);
 
       doRequest.then(
         res => {
