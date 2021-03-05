2b8e715473227938e2c6f7a55bee37e79b216f38
superbeefyboy <superbeefyboy@pm.me>
2021-03-05 20:32:39 -0300

    cache busting

--- src/features/web3/fetchPrice.js
+++ src/features/web3/fetchPrice.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 import { pools } from '../configure/pools';
 
 const endpoints = {
-  bakery: 'https://api.beefy.finance/bakery/price',
+  bakery: 'https://api.beefy.finance/bakery/price?_=1614986441',
   coingecko: 'https://api.coingecko.com/api/v3/simple/price',
-  pancake: 'https://api.beefy.finance/pancake/price',
-  lps: 'https://api.beefy.finance/lps',
+  pancake: 'https://api.beefy.finance/pancake/price?_=1614986441',
+  lps: 'https://api.beefy.finance/lps?_=1614986441',
 };
 
 const CACHE_TIMEOUT_MS = 1 * 60 * 1000; // 1 minute(s)
