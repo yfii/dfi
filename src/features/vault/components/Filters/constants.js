e6558d3e9b80f152255ab62f2043ff5f64427c37
superbeefyboy <superbeefyboy@pm.me>
2021-03-07 17:36:14 -0300

    sort filters

--- src/features/vault/components/Filters/constants.js
+++ src/features/vault/components/Filters/constants.js
@@ -1,6 +1,6 @@
 import { pools } from '../../../configure/pools.js';
 
-const unique = (key) => [...new Set(pools.map(pool => pool[key]).flat().filter(data => data !== undefined))];
+const unique = (key) => [...new Set(pools.map(pool => pool[key]).flat().filter(data => data !== undefined).sort())];
 export const assets = unique('assets');
 export const platforms = unique('platform');
 export const stables = ['BUSD', 'USDT', 'USDC', 'DAI', 'VAI', 'QUSD'];
