5af3d7cf2f1e2b8498f052dab3b2a197561413ae
RM <roman-monk@users.noreply.github.com>
2021-03-10 12:30:40 +0800

    cache

--- src/features/vault/redux/fetchApys.js
+++ src/features/vault/redux/fetchApys.js
@@ -14,7 +14,7 @@ export function fetchApys() {
     });
 
     const promise = new Promise((resolve, reject) => {
-      const doRequest = axios.get(`https://api.beefy.finance/apy?_=1615324223`);
+      const doRequest = axios.get(`https://api.beefy.finance/apy?_=1615324224`);
 
       doRequest.then(
         res => {
