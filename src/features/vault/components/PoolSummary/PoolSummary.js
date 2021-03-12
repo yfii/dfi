60d2391629dd127c1218fb24d017b37898f82249
RM <roman-monk@users.noreply.github.com>
2021-03-12 20:57:26 +0800

    Dont show $ balance until vaults data fetched

--- src/features/vault/components/PoolSummary/PoolSummary.js
+++ src/features/vault/components/PoolSummary/PoolSummary.js
@@ -45,9 +45,9 @@ const PoolSummary = ({
     return state === null ? '' : <PoolPaused message={t(state)} isBoosted={!!launchpool} />;
   };
 
-  const balanceUsd = balanceSingle > 0 ? formatTvl(balanceSingle, pool.oraclePrice) : '';
+  const balanceUsd = balanceSingle && fetchVaultsDataDone > 0 ? formatTvl(balanceSingle, pool.oraclePrice) : '';
   const deposited = byDecimals(sharesBalance.multipliedBy(new BigNumber(pool.pricePerFullShare)), pool.tokenDecimals);
-  const depositedUsd = deposited > 0 ? formatTvl(deposited, pool.oraclePrice) : '';
+  const depositedUsd = deposited && fetchVaultsDataDone > 0 ? formatTvl(deposited, pool.oraclePrice) : '';
 
   return (
     <AccordionSummary
