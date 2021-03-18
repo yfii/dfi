1b29dca701749ad433cea11a20ebc71721b2dcfe
sirbeefalot <sirbeefalot@pm.me>
2021-03-18 16:34:02 -0300

    rm conflict

--- src/features/configure/vault/avalanche_pools.js
+++ src/features/configure/vault/avalanche_pools.js
@@ -1,206 +1,3 @@
-<<<<<<< HEAD:src/features/configure/vault/avalanche_pools.js
-export const avalanchePools = [
-  {
-    id: 'png-sushi-avax',
-    logo: 'avax-pairs/SUSHI-AVAX.svg',
-    name: 'SUSHI-AVAX LP',
-    token: 'SUSHI-AVAX LP',
-    tokenDescription: 'Uses: Pangolin',
-    tokenAddress: '0xd8B262C0676E13100B33590F10564b46eeF652AD',
-    tokenDecimals: 18,
-    tokenDescriptionUrl: '#',
-    earnedToken: 'mooPangolinSUSHI-AVAX',
-    earnedTokenAddress: '0x3094Ab4Af54f5208B867125B5CCeCc94Bc17cbB6',
-    earnContractAddress: '0x3094Ab4Af54f5208B867125B5CCeCc94Bc17cbB6',
-    pricePerFullShare: 1,
-    tvl: 0,
-    oracle: 'lps',
-    oracleId: 'png-sushi-avax',
-    oraclePrice: 0,
-    depositsPaused: false,
-    status: 'active',
-    platform: 'Pangolin',
-    assets: ['SUSHI', 'AVAX'],
-    callFee: 0.25,
-    addLiquidityUrl: 'https://app.pangolin.exchange/#/add/AVAX/0x39cf1BD5f15fb22eC3D9Ff86b0727aFc203427cc',
-  },
-
-  {
-    id: 'png-wbtc-avax',
-    logo: 'avax-pairs/WBTC-AVAX.svg',
-    name: 'WBTC-AVAX LP',
-    token: 'WBTC-AVAX LP',
-    tokenDescription: 'Uses: Pangolin',
-    tokenAddress: '0x7a6131110B82dAcBb5872C7D352BfE071eA6A17C',
-    tokenDecimals: 18,
-    tokenDescriptionUrl: '#',
-    earnedToken: 'mooPangolinWBTC-AVAX',
-    earnedTokenAddress: '0xd5ab3Fac6200B0D8e8d76daED62793026118A78c',
-    earnContractAddress: '0xd5ab3Fac6200B0D8e8d76daED62793026118A78c',
-    pricePerFullShare: 1,
-    tvl: 0,
-    oracle: 'lps',
-    oracleId: 'png-wbtc-avax',
-    oraclePrice: 0,
-    depositsPaused: false,
-    status: 'active',
-    platform: 'Pangolin',
-    assets: ['WBTC', 'AVAX'],
-    callFee: 0.25,
-    addLiquidityUrl: 'https://app.pangolin.exchange/#/add/AVAX/0x408D4cD0ADb7ceBd1F1A1C33A0Ba2098E1295bAB',
-  },
-
-  {
-    id: 'png-link-avax',
-    logo: 'avax-pairs/LINK-AVAX.svg',
-    name: 'LINK-AVAX LP',
-    token: 'LINK-AVAX LP',
-    tokenDescription: 'Uses: Pangolin',
-    tokenAddress: '0xbbC7fFF833D27264AaC8806389E02F717A5506c9',
-    tokenDecimals: 18,
-    tokenDescriptionUrl: '#',
-    earnedToken: 'mooPangolinLINK-AVAX',
-    earnedTokenAddress: '0x6571052b2FB67DF6DD003ED6ed371098A030Eb0d',
-    earnContractAddress: '0x6571052b2FB67DF6DD003ED6ed371098A030Eb0d',
-    pricePerFullShare: 1,
-    tvl: 0,
-    oracle: 'lps',
-    oracleId: 'png-link-avax',
-    oraclePrice: 0,
-    depositsPaused: false,
-    status: 'active',
-    platform: 'Pangolin',
-    assets: ['LINK', 'AVAX'],
-    callFee: 0.25,
-    addLiquidityUrl: 'https://app.pangolin.exchange/#/add/AVAX/0xB3fe5374F67D7a22886A0eE082b2E2f9d2651651',
-  },
-
-  {
-    id: 'png-uni-avax',
-    logo: 'avax-pairs/UNI-AVAX.svg',
-    name: 'UNI-AVAX LP',
-    token: 'UNI-AVAX LP',
-    tokenDescription: 'Uses: Pangolin',
-    tokenAddress: '0x92dC558cB9f8d0473391283EaD77b79b416877cA',
-    tokenDecimals: 18,
-    tokenDescriptionUrl: '#',
-    earnedToken: 'mooPangolinUNI-AVAX',
-    earnedTokenAddress: '0x17657955D954bD7F7315C388D7099af7B0b851FA',
-    earnContractAddress: '0x17657955D954bD7F7315C388D7099af7B0b851FA',
-    pricePerFullShare: 1,
-    tvl: 0,
-    oracle: 'lps',
-    oracleId: 'png-uni-avax',
-    oraclePrice: 0,
-    depositsPaused: false,
-    status: 'active',
-    platform: 'Pangolin',
-    assets: ['UNI', 'AVAX'],
-    callFee: 0.25,
-    addLiquidityUrl: 'https://app.pangolin.exchange/#/add/AVAX/0xf39f9671906d8630812f9d9863bBEf5D523c84Ab',
-  },
-
-  {
-    id: 'png-usdt-png',
-    logo: 'avax-pairs/USDT-PNG.svg',
-    name: 'USDT-PNG LP',
-    token: 'USDT-PNG LP',
-    tokenDescription: 'Uses: Pangolin',
-    tokenAddress: '0xE8AcF438B10A2C09f80aEf3Ef2858F8E758C98F9',
-    tokenDecimals: 18,
-    tokenDescriptionUrl: '#',
-    earnedToken: 'mooPangolinUSDT-PNG',
-    earnedTokenAddress: '0x044e87f30bd9bD961c04028aC69155493E1b9eD0',
-    earnContractAddress: '0x044e87f30bd9bD961c04028aC69155493E1b9eD0',
-    pricePerFullShare: 1,
-    tvl: 0,
-    oracle: 'lps',
-    oracleId: 'png-usdt-png',
-    oraclePrice: 0,
-    depositsPaused: false,
-    status: 'active',
-    platform: 'Pangolin',
-    assets: ['USDT', 'PNG'],
-    callFee: 0.25,
-    addLiquidityUrl: 'https://app.pangolin.exchange/#/add/0x60781C2586D68229fde47564546784ab3fACA982/0xde3A24028580884448a5397872046a019649b084',
-  },
-
-    {
-      id: 'png-eth-avax',
-      logo: 'avax-pairs/ETH-AVAX.svg',
-      name: 'ETH-AVAX LP',
-      token: 'ETH-AVAX LP',
-      tokenDescription: 'Uses: Pangolin',
-      tokenAddress: '0x1aCf1583bEBdCA21C8025E172D8E8f2817343d65',
-      tokenDecimals: 18,
-      tokenDescriptionUrl: '#',
-      earnedToken: 'mooPangolinETH-AVAX',
-      earnedTokenAddress: '0xDA875A511860f2752B891677489d08CaEDac00EA',
-      earnContractAddress: '0xDA875A511860f2752B891677489d08CaEDac00EA',
-      pricePerFullShare: 1,
-      tvl: 0,
-      oracle: 'lps',
-      oracleId: 'png-eth-avax',
-      oraclePrice: 0,
-      depositsPaused: false,
-      status: 'active',
-      platform: 'Pangolin',
-      assets: ['ETH', 'AVAX'],
-      callFee: 0.25,
-      addLiquidityUrl: 'https://app.pangolin.exchange/#/add/AVAX/0xf20d962a6c8f70c731bd838a3a388D7d48fA6e15',
-    },
-
-    {
-      id: 'png-usdt-avax',
-      logo: 'avax-pairs/USDT-AVAX.svg',
-      name: 'USDT-AVAX LP',
-      token: 'USDT-AVAX LP',
-      tokenDescription: 'Uses: Pangolin',
-      tokenAddress: '0x9EE0a4E21bd333a6bb2ab298194320b8DaA26516',
-      tokenDecimals: 18,
-      tokenDescriptionUrl: '#',
-      earnedToken: 'mooPangolinUSDT-AVAX',
-      earnedTokenAddress: '0x7a670e849DB824364d1031DEAfB4cD603144F23D',
-      earnContractAddress: '0x7a670e849DB824364d1031DEAfB4cD603144F23D',
-      pricePerFullShare: 1,
-      tvl: 0,
-      oracle: 'lps',
-      oracleId: 'png-usdt-avax',
-      oraclePrice: 0,
-      depositsPaused: false,
-      status: 'active',
-      platform: 'Pangolin',
-      assets: ['USDT', 'AVAX'],
-      callFee: 0.25,
-      addLiquidityUrl: 'https://app.pangolin.exchange/#/add/AVAX/0xde3A24028580884448a5397872046a019649b084',
-    },
-
-    {
-      id: 'png-png-avax',
-      logo: 'avax-pairs/PNG-AVAX.svg',
-      name: 'PNG-AVAX LP',
-      token: 'PNG-AVAX LP',
-      tokenDescription: 'Uses: Pangolin',
-      tokenAddress: '0xd7538cABBf8605BdE1f4901B47B8D42c61DE0367',
-      tokenDecimals: 18,
-      tokenDescriptionUrl: '#',
-      earnedToken: 'mooPangolinPNG-AVAX',
-      earnedTokenAddress: '0x71b5852857b85D5096d4288AD6d293F217d8e162',
-      earnContractAddress: '0x71b5852857b85D5096d4288AD6d293F217d8e162',
-      pricePerFullShare: 1,
-      tvl: 0,
-      oracle: 'lps',
-      oracleId: 'png-png-avax',
-      oraclePrice: 0,
-      depositsPaused: false,
-      status: 'active',
-      platform: 'Pangolin',
-      assets: ['PNG', 'AVAX'],
-      callFee: 0.25,
-      addLiquidityUrl: 'https://app.pangolin.exchange/#/add/AVAX/0x60781C2586D68229fde47564546784ab3fACA982',
-    },
-  ];
-=======
 export const avalanchePools = [
   {
     id: 'com-com-avax',
@@ -224,7 +21,8 @@ export const avalanchePools = [
     platform: 'Complus',
     assets: ['COM', 'AVAX'],
     callFee: 0.25,
-    addLiquidityUrl: 'https://avadex.complus.exchange/#/add/AVAX/0x3711c397B6c8F7173391361e27e67d72F252cAad',
+    addLiquidityUrl:
+      'https://avadex.complus.exchange/#/add/AVAX/0x3711c397B6c8F7173391361e27e67d72F252cAad',
   },
   {
     id: 'com-dai-avax',
@@ -248,7 +46,8 @@ export const avalanchePools = [
     platform: 'Complus',
     assets: ['DAI', 'AVAX'],
     callFee: 0.25,
-    addLiquidityUrl: 'https://avadex.complus.exchange/#/add/AVAX/0xbA7dEebBFC5fA1100Fb055a87773e1E99Cd3507a',
+    addLiquidityUrl:
+      'https://avadex.complus.exchange/#/add/AVAX/0xbA7dEebBFC5fA1100Fb055a87773e1E99Cd3507a',
   },
 
   {
@@ -273,7 +72,8 @@ export const avalanchePools = [
     platform: 'Pangolin',
     assets: ['SUSHI', 'AVAX'],
     callFee: 0.25,
-    addLiquidityUrl: 'https://app.pangolin.exchange/#/add/AVAX/0x39cf1BD5f15fb22eC3D9Ff86b0727aFc203427cc',
+    addLiquidityUrl:
+      'https://app.pangolin.exchange/#/add/AVAX/0x39cf1BD5f15fb22eC3D9Ff86b0727aFc203427cc',
   },
 
   {
@@ -298,7 +98,8 @@ export const avalanchePools = [
     platform: 'Pangolin',
     assets: ['WBTC', 'AVAX'],
     callFee: 0.25,
-    addLiquidityUrl: 'https://app.pangolin.exchange/#/add/AVAX/0x408D4cD0ADb7ceBd1F1A1C33A0Ba2098E1295bAB',
+    addLiquidityUrl:
+      'https://app.pangolin.exchange/#/add/AVAX/0x408D4cD0ADb7ceBd1F1A1C33A0Ba2098E1295bAB',
   },
 
   {
@@ -323,7 +124,8 @@ export const avalanchePools = [
     platform: 'Pangolin',
     assets: ['LINK', 'AVAX'],
     callFee: 0.25,
-    addLiquidityUrl: 'https://app.pangolin.exchange/#/add/AVAX/0xB3fe5374F67D7a22886A0eE082b2E2f9d2651651',
+    addLiquidityUrl:
+      'https://app.pangolin.exchange/#/add/AVAX/0xB3fe5374F67D7a22886A0eE082b2E2f9d2651651',
   },
 
   {
@@ -348,7 +150,8 @@ export const avalanchePools = [
     platform: 'Pangolin',
     assets: ['UNI', 'AVAX'],
     callFee: 0.25,
-    addLiquidityUrl: 'https://app.pangolin.exchange/#/add/AVAX/0xf39f9671906d8630812f9d9863bBEf5D523c84Ab',
+    addLiquidityUrl:
+      'https://app.pangolin.exchange/#/add/AVAX/0xf39f9671906d8630812f9d9863bBEf5D523c84Ab',
   },
 
   {
@@ -373,7 +176,8 @@ export const avalanchePools = [
     platform: 'Pangolin',
     assets: ['USDT', 'PNG'],
     callFee: 0.25,
-    addLiquidityUrl: 'https://app.pangolin.exchange/#/add/0x60781C2586D68229fde47564546784ab3fACA982/0xde3A24028580884448a5397872046a019649b084',
+    addLiquidityUrl:
+      'https://app.pangolin.exchange/#/add/0x60781C2586D68229fde47564546784ab3fACA982/0xde3A24028580884448a5397872046a019649b084',
   },
 
   {
@@ -398,7 +202,8 @@ export const avalanchePools = [
     platform: 'Pangolin',
     assets: ['ETH', 'AVAX'],
     callFee: 0.25,
-    addLiquidityUrl: 'https://app.pangolin.exchange/#/add/AVAX/0xf20d962a6c8f70c731bd838a3a388D7d48fA6e15',
+    addLiquidityUrl:
+      'https://app.pangolin.exchange/#/add/AVAX/0xf20d962a6c8f70c731bd838a3a388D7d48fA6e15',
   },
 
   {
@@ -423,7 +228,8 @@ export const avalanchePools = [
     platform: 'Pangolin',
     assets: ['USDT', 'AVAX'],
     callFee: 0.25,
-    addLiquidityUrl: 'https://app.pangolin.exchange/#/add/AVAX/0xde3A24028580884448a5397872046a019649b084',
+    addLiquidityUrl:
+      'https://app.pangolin.exchange/#/add/AVAX/0xde3A24028580884448a5397872046a019649b084',
   },
 
   {
@@ -448,7 +254,7 @@ export const avalanchePools = [
     platform: 'Pangolin',
     assets: ['PNG', 'AVAX'],
     callFee: 0.25,
-    addLiquidityUrl: 'https://app.pangolin.exchange/#/add/AVAX/0x60781C2586D68229fde47564546784ab3fACA982',
+    addLiquidityUrl:
+      'https://app.pangolin.exchange/#/add/AVAX/0x60781C2586D68229fde47564546784ab3fACA982',
   },
 ];
->>>>>>> master:src/features/configure/avalanche_pools.js
