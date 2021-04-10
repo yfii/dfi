a55565735ef32f91457496e12a7e43fbea561b8f
RM <roman-monk@users.noreply.github.com>
2021-04-10 17:39:28 +0800

    Exclude duplicates from user deposited tvl

--- src/features/vault/hooks/usePoolsTvl.js
+++ src/features/vault/hooks/usePoolsTvl.js
@@ -1,15 +1,12 @@
+import BigNumber from 'bignumber.js';
 import { useState, useEffect } from 'react';
+import { byDecimals } from '../../helpers/bignumber';
 
 const usePoolsTvl = pools => {
   const [poolsTvl, setPoolsTvl] = useState(0);
 
   useEffect(() => {
     let globalTvl = 0;
-
-    const isUniqueEarnContract = (pool, index, pools) => {
-      const earnContractAddress = pool.earnContractAddress;
-      return pools.findIndex(p => p.earnContractAddress === earnContractAddress) === index;
-    };
 
     pools
       .filter(p => p.status === 'active')
@@ -24,4 +21,32 @@ const usePoolsTvl = pools => {
   return { poolsTvl };
 };
 
-export default usePoolsTvl;
+const useUserTvl = (pools, tokens) => {
+  const [userTvl, setUserTvl] = useState(0);
+
+  useEffect(() => {
+    let userTvl = 0;
+
+    pools.filter(isUniqueEarnContract).forEach(pool => {
+      const sharesBalance = new BigNumber(tokens[pool.earnedToken].tokenBalance);
+      if (sharesBalance > 0) {
+        const deposited = byDecimals(
+          sharesBalance.multipliedBy(new BigNumber(pool.pricePerFullShare)),
+          pool.tokenDecimals,
+        );
+        userTvl += deposited * pool.oraclePrice;
+      }
+    });
+
+    setUserTvl(userTvl);
+  }, [pools, tokens]);
+
+  return { userTvl };
+};
+
+const isUniqueEarnContract = (pool, index, pools) => {
+  const earnContractAddress = pool.earnContractAddress;
+  return pools.findIndex(p => p.earnContractAddress === earnContractAddress) === index;
+};
+
+export { usePoolsTvl, useUserTvl };
