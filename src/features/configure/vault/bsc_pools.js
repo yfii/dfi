99fa8ef3c4dee017afdaa7fd7bb910f30d222fa1
MirthFutures <admin@betswapexchange.com>
2021-04-17 20:23:30 -0400

    fix btc filter

--- src/features/configure/vault/bsc_pools.js
+++ src/features/configure/vault/bsc_pools.js
@@ -278,7 +278,7 @@ export const bscPools = [
     depositsPaused: false,
     status: 'active',
     platform: 'Autofarm',
-    assets: ['BTCB', 'USDT'],
+    assets: ['BTC', 'USDT'],
     callFee: 0.5,
     addLiquidityUrl:
       'https://bsc.mdex.com/#/add/0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c/0x55d398326f99059fF775485246999027B3197955',
@@ -306,7 +306,7 @@ export const bscPools = [
     depositsPaused: false,
     status: 'active',
     platform: 'Autofarm',
-    assets: ['BTCB', 'ETH'],
+    assets: ['BTC', 'ETH'],
     callFee: 0.5,
     addLiquidityUrl:
       'https://bsc.mdex.com/#/add/0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c/0x2170Ed0880ac9A755fd29B2688956BD959F933F8',
