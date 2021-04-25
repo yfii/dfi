73ea2a7223746beec9354cd26d72d7cbef692eeb
RM <roman-monk@users.noreply.github.com>
2021-04-25 19:50:57 +0800

    Add DEGO, GUM, pBTC, BIFI, BELT, BELLA, bMXX, BOR, bOPEN Pancake v2

--- src/features/vault/redux/fetchApys.js
+++ src/features/vault/redux/fetchApys.js
@@ -14,7 +14,7 @@ export function fetchApys() {
     });
 
     const promise = new Promise((resolve, reject) => {
-      const doRequest = axios.get(`https://api.beefy.finance/apy?_=1618266466`);
+      const doRequest = axios.get(`https://api.beefy.finance/apy?_=1618266566`);
       
       doRequest.then(
         res => {
