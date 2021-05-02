bf87a17f6fd489bee7332eb4559ba72ebaaa8987
sirbeefalot <sirbeefalot@pm.me>
2021-05-01 21:19:27 -0300

    cache bust

--- src/features/vault/redux/fetchApys.js
+++ src/features/vault/redux/fetchApys.js
@@ -14,8 +14,8 @@ export function fetchApys() {
     });
 
     const promise = new Promise((resolve, reject) => {
-      const doRequest = axios.get(`https://api.beefy.finance/apy?_=1619716666`);
-      
+      const doRequest = axios.get(`https://api.beefy.finance/apy?_=1619716667`);
+
       doRequest.then(
         res => {
           dispatch({
