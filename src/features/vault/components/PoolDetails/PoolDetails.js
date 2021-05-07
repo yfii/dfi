9b42f62728eb7a7eea8853d1ace1712bfecfeb84
Donnie Kim <mobaddicts@hotmail.com>
2021-05-07 09:59:01 -0400

    add balance, deposit amount, customize mobile layout

--- src/features/vault/components/PoolDetails/PoolDetails.js
+++ src/features/vault/components/PoolDetails/PoolDetails.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
+import { useSelector } from 'react-redux';
 import Divider from '@material-ui/core/Divider';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
@@ -21,13 +22,28 @@ const FETCH_INTERVAL_MS = 30 * 1000;
 
 const useStyles = makeStyles(styles);
 
+const formatDecimals = number => {
+  return number >= 10 ? number.toFixed(4) : number.isEqualTo(0) ? 0 : number.toFixed(8);
+};
+
 const PoolDetails = ({ vaultId }) => {
   const { t } = useTranslation();
   const { web3, address } = useConnectWallet();
   const { pools, fetchVaultsData, fetchVaultsDataDone } = useFetchVaultsData();
-  const { tokens, fetchBalances } = useFetchBalances();
+  const { tokens, fetchBalances, fetchBalancesDone } = useFetchBalances();
   const { apys, fetchApys, fetchApysDone } = useFetchApys();
   const pool = pools.find(p => p.id === vaultId);
+  const stake = useSelector((state) => state.stake.pools);
+
+  const launchpool = useMemo(() => {
+    const timestamp = Math.floor(Date.now() / 1000);
+    for (let index in stake) {
+      if(stake[index].token === pool.earnedToken && stake[index].periodFinish >= timestamp) {
+        stake[index].poolIndex = Number(index) + 1;
+        return stake[index];
+      }
+    }
+  }, [pool, stake]);
 
   useEffect(() => {
     if (address && web3) {
@@ -46,24 +62,26 @@ const PoolDetails = ({ vaultId }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [address, web3, fetchBalances, fetchVaultsData]);
 
-  const classes = useStyles(pool);
+  const vaultStateTitle = useMemo(() => {
+    let state =
+      pool.status === 'eol'
+        ? t('Vault-DepositsRetiredTitle')
+        : pool.paused
+        ? t('Vault-DepositsPausedTitle')
+        : null;
 
-  if (!fetchVaultsDataDone) {
-    return (
-      <div className={classes.container}>
-        <HomeLink />
-        <h1>Loading Vault...</h1>
-      </div>
-    );
-  } else if (!pool) {
-    return (
-      <div className={classes.container}>
-        <HomeLink />
-        <h1>Vault {vaultId} not found</h1>
-      </div>
-    );
-  }
+    if (launchpool) {
+      state = t('Stake-BoostedBy', { name: launchpool.name }) ;
+    }
 
+    if (pool.experimental) {
+      state = t('Vault-Experimental') ;
+    }
+
+    return state === null ? '' : <Paper variant="outlined" elevation={5} className={classes.status}>{state}</Paper>;
+  }, [pool, launchpool]);
+
+  /*
   const vaultStateTitle =
     pool.status === 'eol' ? (
       <>
@@ -81,17 +99,44 @@ const PoolDetails = ({ vaultId }) => {
       {vaultStateTitle}
     </Paper>
   );
-
+  */
   const balanceSingle = byDecimals(tokens[pool.token].tokenBalance, pool.tokenDecimals);
   const sharesBalance = new BigNumber(tokens[pool.earnedToken].tokenBalance);
   const apy = apys[pool.id] || 0;
+
+  const balanceUsd =
+    balanceSingle > 0 && fetchVaultsDataDone ? formatTvl(balanceSingle, pool.oraclePrice) : '';
+  const deposited = byDecimals(
+    sharesBalance.multipliedBy(new BigNumber(pool.pricePerFullShare)),
+    pool.tokenDecimals
+  );
+  const depositedUsd =
+    deposited > 0 && fetchVaultsDataDone ? formatTvl(deposited, pool.oraclePrice) : '';
+  const mobilePadding = balanceSingle > 0 || deposited > 0 ? '24px' : '10px';
+
+  const classes = useStyles(pool);
+  if (!fetchVaultsDataDone) {
+    return (
+      <div className={classes.container}>
+        <HomeLink />
+        <h1>Loading Vault...</h1>
+      </div>
+    );
+  } else if (!pool) {
+    return (
+      <div className={classes.container}>
+        <HomeLink />
+        <h1>Vault {vaultId} not found</h1>
+      </div>
+    );
+  }
 
   return (
     <>
       <HomeLink />
       <div className={classes.container}>
         <h1 className={classes.heading}>Vault Details</h1>
-        {vaultState}
+        {vaultStateTitle}
         <Grid className={classes.summary} container justify="space-around" xs={12} spacing={0}>
           <PoolTitle
             name={pool.name}
@@ -100,9 +145,26 @@ const PoolDetails = ({ vaultId }) => {
             url={pool.tokenDescriptionUrl}
             columns={6}
           />
-          <Grid item xs={4}>
+          <Grid item md={8} xs={7}>
             <Grid item container justify="space-between">
               <Hidden smDown>
+                <LabeledStat
+                  value={formatDecimals(balanceSingle)}
+                  subvalue={balanceUsd}
+                  label={t('Vault-Balance')}
+                  isLoading={!fetchBalancesDone}
+                  xs={5}
+                  md={3}
+                />
+                <LabeledStat
+                  value={formatDecimals(deposited)}
+                  subvalue={depositedUsd}
+                  label={t('Vault-Deposited')}
+                  isLoading={!fetchBalancesDone}
+                  xs={5}
+                  md={3}
+                  align="start"
+                />
                 <LabeledStat
                   value={formatApy(apy)}
                   label={t('Vault-APY')}
@@ -125,6 +187,53 @@ const PoolDetails = ({ vaultId }) => {
                   xs={5}
                   md={2}
                 />
+              </Hidden>
+              <Hidden mdUp>
+                <Grid
+                  item
+                  xs={12}
+                  style={{ display: 'flex', paddingTop: mobilePadding }}
+                  className={classes.mobilePadding}
+                >
+                  <LabeledStat
+                    value={formatDecimals(balanceSingle)}
+                    subvalue={balanceUsd}
+                    label={t('Vault-Balance')}
+                    isLoading={!fetchBalancesDone}
+                    xs={6}
+                  />
+                  <LabeledStat
+                    value={formatDecimals(deposited)}
+                    subvalue={depositedUsd}
+                    label={t('Vault-Deposited')}
+                    isLoading={!fetchBalancesDone}
+                    xs={6}
+                    align="start"
+                  />
+                </Grid>
+                <Grid item xs={12} style={{ display: 'flex', paddingTop: '20px' }}>
+                  <LabeledStat
+                    value={formatApy(apy)}
+                    label={t('Vault-APY')}
+                    isLoading={!fetchApysDone}
+                    boosted={launchpool ? formatApy(launchpool.apy + apy) : ''}
+                    xs={4}
+                    align="start"
+                  />
+                  <LabeledStat
+                    value={calcDaily(apy)}
+                    label={t('Vault-APYDaily')}
+                    isLoading={!fetchApysDone}
+                    boosted={launchpool ? calcDaily(launchpool.apy + apy) : ''}
+                    xs={4}
+                  />
+                  <LabeledStat
+                    value={formatTvl(pool.tvl, pool.oraclePrice)}
+                    label={t('Vault-TVL')}
+                    isLoading={!fetchVaultsDataDone}
+                    xs={4}
+                  />
+                </Grid>
               </Hidden>
             </Grid>
           </Grid>
