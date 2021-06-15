39d4c18f88b064c70ce422ed15f5843eb84a0539
sirbeefalot <sirbeefalot@pm.me>
2021-06-15 16:45:50 -0300

    Populate required extra addresses for validation.

--- scripts/validatePools.js
+++ scripts/validatePools.js
@@ -1,119 +1,222 @@
 // To run: yarn validate
+import { MultiCall } from 'eth-multicall';
+import { addressBook } from 'blockchain-addressbook';
+import Web3 from 'web3';
+
 import { isValidChecksumAddress, maybeChecksumAddress } from './utils.js';
 import { bscPools } from '../src/features/configure/vault/bsc_pools.js';
 import { hecoPools } from '../src/features/configure/vault/heco_pools.js';
 import { avalanchePools } from '../src/features/configure/vault/avalanche_pools.js';
 import { polygonPools } from '../src/features/configure/vault/polygon_pools.js';
 import { fantomPools } from '../src/features/configure/vault/fantom_pools.js';
+import { vaultABI, strategyABI } from '../src/features/configure/abi.js';
 
 const chainPools = {
-  bscPools,
-  hecoPools,
-  avalanchePools,
-  polygonPools,
-  fantomPools,
+  bsc: bscPools,
+  heco: hecoPools,
+  avax: avalanchePools,
+  polygon: polygonPools,
+  fantom: fantomPools,
 };
 
-const addressFields = ['tokenAddress', 'earnedTokenAddress', 'earnContractAddress'];
+const chainRpcs = {
+  bsc: process.env.BSC_RPC,
+  heco: process.env.HECO_RPC,
+  avax: process.env.AVAX_RPC,
+  polygon: process.env.POLYGON_RPC,
+  fantom: process.env.FANTOM_RPC,
+};
 
-const allowedEarnSameToken = new Set(['venus-wbnb']);
+const validatePools = async () => {
+  const addressFields = ['tokenAddress', 'earnedTokenAddress', 'earnContractAddress'];
 
-// Outputs alphabetical list of platforms per chain (useful to make sure they are consistently named)
-const outputPlatformSummary = process.argv.includes('--platform-summary');
+  const allowedEarnSameToken = new Set(['venus-wbnb']);
 
-let exitCode = 0;
+  // Outputs alphabetical list of platforms per chain (useful to make sure they are consistently named)
+  const outputPlatformSummary = process.argv.includes('--platform-summary');
 
-for (const [chain, pools] of Object.entries(chainPools)) {
-  console.log(`Validating ${pools.length} pools in ${chain}...`);
+  let exitCode = 0;
 
-  const uniquePoolId = new Set();
-  const uniqueEarnedToken = new Set();
-  const uniqueEarnedTokenAddress = new Set();
-  const uniqueOracleId = new Set();
-  const platformCounts = {};
-  let activePools = 0;
+  for (let [chain, pools] of Object.entries(chainPools)) {
+    console.log(`Validating ${pools.length} pools in ${chain}...`);
 
-  pools.forEach(pool => {
-    // Errors, should not proceed with build
-    if (uniquePoolId.has(pool.id)) {
-      console.error(`Error: ${pool.id} : Pool id duplicated: ${pool.id}`);
-      exitCode = 1;
-    }
+    const uniquePoolId = new Set();
+    const uniqueEarnedToken = new Set();
+    const uniqueEarnedTokenAddress = new Set();
+    const uniqueOracleId = new Set();
+    const platformCounts = {};
+    let activePools = 0;
 
-    if (uniqueEarnedToken.has(pool.earnedToken) && !allowedEarnSameToken.has(pool.id)) {
-      console.error(`Error: ${pool.id} : Pool earnedToken duplicated: ${pool.earnedToken}`);
-      exitCode = 1;
-    }
+    pools.forEach(pool => {
+      // Errors, should not proceed with build
+      if (uniquePoolId.has(pool.id)) {
+        console.error(`Error: ${pool.id} : Pool id duplicated: ${pool.id}`);
+        exitCode = 1;
+      }
 
-    if (
-      uniqueEarnedTokenAddress.has(pool.earnedTokenAddress) &&
-      !allowedEarnSameToken.has(pool.id)
-    ) {
-      console.error(
-        `Error: ${pool.id} : Pool earnedTokenAddress duplicated: ${pool.earnedTokenAddress}`
-      );
-      exitCode = 1;
-    }
+      if (uniqueEarnedToken.has(pool.earnedToken) && !allowedEarnSameToken.has(pool.id)) {
+        console.error(`Error: ${pool.id} : Pool earnedToken duplicated: ${pool.earnedToken}`);
+        exitCode = 1;
+      }
 
-    if (pool.earnedTokenAddress !== pool.earnContractAddress) {
-      console.error(
-        `Error: ${pool.id} : Pool earnedTokenAddress not same as earnContractAddress: ${pool.earnedTokenAddress} != ${pool.earnContractAddress}`
-      );
-      exitCode = 1;
-    }
-
-    if (!pool.tokenDescription) {
-      console.error(
-        `Error: ${pool.id} : Pool tokenDescription missing - required for UI: vault card`
-      );
-      exitCode = 1;
-    }
-
-    if (!pool.platform) {
-      console.error(
-        `Error: ${pool.id} : Pool platform missing - required for UI: filter (Use 'Other' if necessary)`
-      );
-      exitCode = 1;
-    } else {
-      platformCounts[pool.platform] = platformCounts.hasOwnProperty(pool.platform)
-        ? platformCounts[pool.platform] + 1
-        : 1;
-    }
-
-    addressFields.forEach(field => {
-      if (pool.hasOwnProperty(field) && !isValidChecksumAddress(pool[field])) {
-        const maybeValid = maybeChecksumAddress(pool[field]);
+      if (
+        uniqueEarnedTokenAddress.has(pool.earnedTokenAddress) &&
+        !allowedEarnSameToken.has(pool.id)
+      ) {
         console.error(
-          `Error: ${pool.id} : ${field} requires checksum - ${
-            maybeValid ? `\n\t${field}: '${maybeValid}',` : 'it is invalid'
-          }`
+          `Error: ${pool.id} : Pool earnedTokenAddress duplicated: ${pool.earnedTokenAddress}`
         );
         exitCode = 1;
       }
+
+      if (pool.earnedTokenAddress !== pool.earnContractAddress) {
+        console.error(
+          `Error: ${pool.id} : Pool earnedTokenAddress not same as earnContractAddress: ${pool.earnedTokenAddress} != ${pool.earnContractAddress}`
+        );
+        exitCode = 1;
+      }
+
+      if (!pool.tokenDescription) {
+        console.error(
+          `Error: ${pool.id} : Pool tokenDescription missing - required for UI: vault card`
+        );
+        exitCode = 1;
+      }
+
+      if (!pool.platform) {
+        console.error(
+          `Error: ${pool.id} : Pool platform missing - required for UI: filter (Use 'Other' if necessary)`
+        );
+        exitCode = 1;
+      } else {
+        platformCounts[pool.platform] = platformCounts.hasOwnProperty(pool.platform)
+          ? platformCounts[pool.platform] + 1
+          : 1;
+      }
+
+      addressFields.forEach(field => {
+        if (pool.hasOwnProperty(field) && !isValidChecksumAddress(pool[field])) {
+          const maybeValid = maybeChecksumAddress(pool[field]);
+          console.error(
+            `Error: ${pool.id} : ${field} requires checksum - ${
+              maybeValid ? `\n\t${field}: '${maybeValid}',` : 'it is invalid'
+            }`
+          );
+          exitCode = 1;
+        }
+      });
+
+      if (pool.status === 'active') {
+        activePools++;
+      }
+
+      uniquePoolId.add(pool.id);
+      uniqueEarnedToken.add(pool.earnedToken);
+      uniqueEarnedTokenAddress.add(pool.earnedTokenAddress);
+      uniqueOracleId.add(pool.oracleId);
     });
 
-    if (pool.status === 'active') {
-      activePools++;
+    if (outputPlatformSummary) {
+      console.log(
+        `Platforms: \n${Object.entries(platformCounts)
+          .sort(([platformA], [platformB]) =>
+            platformA.localeCompare(platformB, 'en', { sensitivity: 'base' })
+          )
+          .map(([platform, count]) => `\t${platform} (${count})`)
+          .join('\n')}`
+      );
     }
 
-    uniquePoolId.add(pool.id);
-    uniqueEarnedToken.add(pool.earnedToken);
-    uniqueEarnedTokenAddress.add(pool.earnedTokenAddress);
-    uniqueOracleId.add(pool.oracleId);
-  });
+    const web3 = new Web3(chainRpcs[chain]);
 
-  if (outputPlatformSummary) {
-    console.log(
-      `Platforms: \n${Object.entries(platformCounts)
-        .sort(([platformA], [platformB]) =>
-          platformA.localeCompare(platformB, 'en', { sensitivity: 'base' })
-        )
-        .map(([platform, count]) => `\t${platform} (${count})`)
-        .join('\n')}`
-    );
+    pools = await populateStrategyAddrs(chain, pools, web3);
+    pools = await populateKeepers(chain, pools, web3);
+    pools = await populateBeefyFeeRecipients(chain, pools, web3);
+    pools = await populateOwners(chain, pools, web3);
+
+    // Validate
+
+    console.log(`Active pools: ${activePools}/${pools.length}\n`);
   }
 
-  console.log(`Active pools: ${activePools}/${pools.length}\n`);
-}
+  return exitCode;
+};
 
+const populateStrategyAddrs = async (chain, pools, web3) => {
+  const multicall = new MultiCall(web3, addressBook[chain].platforms.beefyfinance.multicall);
+
+  const calls = pools.map(pool => {
+    const vaultContract = new web3.eth.Contract(vaultABI, pool.earnContractAddress);
+    return {
+      strategy: vaultContract.methods.strategy(),
+    };
+  });
+
+  const [results] = await multicall.all([calls]);
+
+  return pools.map((pool, i) => {
+    return { ...pool, strategy: results[i].strategy };
+  });
+};
+
+const populateKeepers = async (chain, pools, web3) => {
+  const multicall = new MultiCall(web3, addressBook[chain].platforms.beefyfinance.multicall);
+
+  const calls = pools.map(pool => {
+    const stratContract = new web3.eth.Contract(strategyABI, pool.strategy);
+    return {
+      keeper: stratContract.methods.keeper(),
+    };
+  });
+
+  const [results] = await multicall.all([calls]);
+
+  return pools.map((pool, i) => {
+    return { ...pool, keeper: results[i].keeper };
+  });
+};
+
+const populateBeefyFeeRecipients = async (chain, pools, web3) => {
+  const multicall = new MultiCall(web3, addressBook[chain].platforms.beefyfinance.multicall);
+
+  const calls = pools.map(pool => {
+    const stratContract = new web3.eth.Contract(strategyABI, pool.strategy);
+    return {
+      beefyFeeRecipient: stratContract.methods.beefyFeeRecipient(),
+    };
+  });
+
+  const [results] = await multicall.all([calls]);
+
+  return pools.map((pool, i) => {
+    return { ...pool, beefyFeeRecipient: results[i].beefyFeeRecipient };
+  });
+};
+
+const populateOwners = async (chain, pools, web3) => {
+  const multicall = new MultiCall(web3, addressBook[chain].platforms.beefyfinance.multicall);
+
+  const vaultCalls = pools.map(pool => {
+    const vaultContract = new web3.eth.Contract(vaultABI, pool.earnContractAddress);
+    return {
+      owner: vaultContract.methods.owner(),
+    };
+  });
+
+  const stratCalls = pools.map(pool => {
+    const stratContract = new web3.eth.Contract(strategyABI, pool.strategy);
+    return {
+      owner: stratContract.methods.owner(),
+    };
+  });
+
+  const [vaultResults] = await multicall.all([vaultCalls]);
+  const [stratResults] = await multicall.all([stratCalls]);
+
+  return pools.map((pool, i) => {
+    return { ...pool, vaultOwner: vaultResults[i].owner, stratOwner: stratResults[i].owner };
+  });
+};
+
+const exitCode = await validatePools();
 process.exit(exitCode);
