361b11efdc3cad7d0ffa193623ea026102b6cf0c
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2021-07-03 13:30:35 +0100

    use cache

--- src/features/vault/components/PoolSummary/PoolTitle/PoolTitle.js
+++ src/features/vault/components/PoolSummary/PoolTitle/PoolTitle.js
@@ -14,9 +14,14 @@ const singleAssetRequire = require.context('images/single-assets', false, /\.(sv
 const singleAssets = Object.fromEntries(
   singleAssetRequire.keys().map(path => [path.substring(2, path.lastIndexOf('.')), path])
 );
+const singleAssetCache = {};
 const singleAsset = symbol => {
+  if (symbol in singleAssetCache) {
+    return singleAssetCache[symbol];
+  }
+
   if (symbol in singleAssets) {
-    return singleAssetRequire(singleAssets[symbol]);
+    return (singleAssetCache[symbol] = singleAssetRequire(singleAssets[symbol]));
   }
 
   throw new Error(`Image required for '${symbol}' token in 'images/single-assets/'`);
