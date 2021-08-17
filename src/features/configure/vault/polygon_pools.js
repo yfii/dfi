cad45152b3061d6035bc6359bcaf649f979ef86c
RM <roman-monk@users.noreply.github.com>
2021-08-17 14:24:38 +0800

    eol Dopple

--- src/features/configure/vault/polygon_pools.js
+++ src/features/configure/vault/polygon_pools.js
@@ -4579,7 +4579,6 @@ export const polygonPools = [
     platform: 'QuickSwap',
     assets: ['USDC', 'USDT'],
     callFee: 0.5,
-    withdrawalFee: '0.01%',
     addLiquidityUrl:
       'https://quickswap.exchange/#/add/0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174/0xc2132D05D31c914a87C6611C10748AEb04B58e8F',
     buyTokenUrl:
