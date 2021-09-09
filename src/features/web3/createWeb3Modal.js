1e10424a15fcf6848d9f2689e86f528bc1621c14
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2021-09-09 17:54:40 +0100

    Web3Modal: Clear cache provider on network change

--- src/features/web3/createWeb3Modal.js
+++ src/features/web3/createWeb3Modal.js
@@ -2,4 +2,13 @@ import Web3Modal from 'web3modal';
 
 import { getNetworkConnectors } from 'features/helpers/getNetworkData';
 
-export const createWeb3Modal = t => new Web3Modal(getNetworkConnectors(t));
+export const createWeb3Modal = t => {
+  const connectors = getNetworkConnectors(t);
+  const modal = new Web3Modal(connectors);
+
+  if (modal.cachedProvider && !(modal.cachedProvider in connectors.providerOptions)) {
+    modal.clearCachedProvider();
+  }
+
+  return modal;
+};
