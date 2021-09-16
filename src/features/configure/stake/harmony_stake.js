cdf14d8afef0dd7c4c208d7dc13c0b469948b78b
sirbeefalot <sirbeefalot@pm.me>
2021-09-16 20:01:24 -0300

    update earnedToken name

--- src/features/configure/stake/harmony_stake.js
+++ src/features/configure/stake/harmony_stake.js
@@ -10,7 +10,7 @@ export const harmonyStakePools = [
     tokenAddress: '0x6aB6d61428fde76768D7b45D8BFeec19c6eF91A8',
     tokenOracle: 'tokens',
     tokenOracleId: 'BIFI',
-    earnedToken: 'ONE',
+    earnedToken: 'WONE',
     earnedTokenDecimals: 18,
     earnedTokenAddress: '0xcF664087a5bB0237a0BAd6742852ec6c8d69A27a',
     earnContractAddress: '0x5B96bbAca98D777cb736dd89A519015315E00D02',
