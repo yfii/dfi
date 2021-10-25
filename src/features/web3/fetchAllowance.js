2f9dc5a33468745e1fe7f99b06ad13862f6ca28b
RM <roman-monk@users.noreply.github.com>
2021-10-25 23:28:32 +0800

    Increase approve value to 8tr

--- src/features/web3/fetchAllowance.js
+++ src/features/web3/fetchAllowance.js
@@ -2,7 +2,7 @@ import BigNumber from 'bignumber.js';
 
 export const fetchAllowance = async ({ web3, address, contract, contractAddress }) => {
   if (!contract) {
-    return 80000000000;
+    return 8000000000000;
   }
   const balance = await contract.methods
     .allowance(address, contractAddress)
