0d09365b01e6f60b9e691300ea16f0cbea4d9c74
sircryptomaxi <87971154+sircryptomaxi@users.noreply.github.com>
2022-02-02 21:35:25 +0000

    Update mint link

--- src/features/configure/vault/fantom_pools.js
+++ src/features/configure/vault/fantom_pools.js
@@ -64,7 +64,7 @@ export const fantomPools = [
     ],
     stratType: 'StratLP',
     withdrawalFee: '0%',
-    mintTokenUrl: 'https://beta.beefy.finance/#/vault/beefy-binspirit',
+    mintTokenUrl: 'https://beta.beefy.finance/#/fantom/vault/beefy-binspirit',
     buyTokenUrl: 'https://swap.spiritswap.finance/#/exchange/swap/0x44e314190D9E4cE6d4C0903459204F8E21ff940A',
   },
   {
