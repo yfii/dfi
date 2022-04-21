daf48a565306468b4bb2128ee3689e71a6c9f385
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2022-04-22 00:25:22 +0100

    Temp disable cakev2-btcb-bnb deposits + withdraws (#1190)

--- src/features/vault/components/PoolActions/PoolActions.js
+++ src/features/vault/components/PoolActions/PoolActions.js
@@ -6,14 +6,21 @@ import WithdrawSection from '../PoolDetails/WithdrawSection/WithdrawSection';
 import HarvestSection from '../PoolDetails/HarvestSection/HarvestSection';
 import { shouldHideFromHarvest } from '../../../helpers/utils';
 import { NetworkRequired } from '../../../../components/NetworkRequired/NetworkRequired';
+import { Box } from '@material-ui/core';
 
 const PoolActions = ({ pool, balanceSingle, index, sharesBalance }) => {
   return (
     <NetworkRequired inline>
       <Grid container>
-        <DepositSection index={index} pool={pool} balanceSingle={balanceSingle} />
-        <WithdrawSection index={index} pool={pool} sharesBalance={sharesBalance} />
-        {shouldHideFromHarvest(pool.id) ? '' : <HarvestSection index={index} pool={pool} />}
+        {pool.id === 'cakev2-btcb-bnb' ? (
+          <Box p={3}>{'Hang tight, this vault is being upgraded and will be back soon.'}</Box>
+        ) : (
+          <>
+            <DepositSection index={index} pool={pool} balanceSingle={balanceSingle} />
+            <WithdrawSection index={index} pool={pool} sharesBalance={sharesBalance} />
+            {shouldHideFromHarvest(pool.id) ? '' : <HarvestSection index={index} pool={pool} />}
+          </>
+        )}
       </Grid>
     </NetworkRequired>
   );
